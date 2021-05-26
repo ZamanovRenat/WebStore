9ff23b942ad35f49df5f236082bcc3d2b48824e0
ZamanovRenat <zamanov.renat@mail.ru>
2021-05-26 21:24:46 +0300

    Добавлена страница редактирования сотрудника

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
+        private readonly List<Employee> __Employees = new()
         {
             new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
             new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
             new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
         };
+
         public IActionResult Index()
         {
             return View();
@@ -37,6 +36,29 @@ namespace WebStore.Controllers
                 return NotFound();
 
             return View(employee);
+        }
+
+
+        /// <summary>
+        /// Добавление или редактирование сотрудника
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        public IActionResult Edit(int? id)
+        {
+            Employee model;
+            if (id.HasValue)
+            {
+                model = __Employees.FirstOrDefault(a => a.Id.Equals(id)); 
+                if (model is null)
+                    return NotFound(); // возвращаем результат 404 Not Found
+            }
+            else
+            {
+                model = new Employee();
+            }
+            return View(model);
         }
     }
 }
