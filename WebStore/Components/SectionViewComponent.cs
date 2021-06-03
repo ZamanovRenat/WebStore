46abb45469e89ebee34f2a05f41b8e010c64ce4c
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-03 19:02:37 +0300

    Секция брендов и категорий при отображении на всех представления

---
+++ WebStore/Components/SectionViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class SectionViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
