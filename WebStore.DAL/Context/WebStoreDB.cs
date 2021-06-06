d524f971ae55ddb497032f9973b312b5b273f0d2
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-06 15:06:47 +0300

    Включен проек DAL

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
+        {
+            
+        }
+    }
+}
