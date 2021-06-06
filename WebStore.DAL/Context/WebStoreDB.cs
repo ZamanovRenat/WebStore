9bdbb6204cdac41b9d941444a23caf331169ac19
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-06 15:31:21 +0300

    Контекст БД для товаров, брэндов, секций

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -6,6 +6,8 @@ namespace WebStore.DAL.Context
     public class WebStoreDB : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
         public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
         {
             
