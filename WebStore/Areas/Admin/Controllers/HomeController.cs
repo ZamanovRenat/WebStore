03461eca1e286dad0a8a83296035657174ff2448
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-16 19:28:02 +0300

    Добавление контроллера управления товаров сайта в область администратора

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("admin")]
+    [Authorize(Roles = Role.Administrators)]
     public class HomeController : Controller
     {
         public IActionResult Index()
