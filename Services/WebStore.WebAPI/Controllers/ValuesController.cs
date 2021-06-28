4e3094f96101dafb9e92a9af125aec763775719c
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-28 22:39:46 +0300

    Контроллер тестового WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    [Route("api/values")]
+    public class ValuesController : ControllerBase
+    {
+        private static List<string> __Values = Enumerable
+            .Range(1, 10)
+            .Select(i => $"Value-{i}")
+            .ToList();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(__Values);
+
+        [HttpGet("count")]
+        public IActionResult GetCount() => Ok(__Values.Count);
+
+        [HttpGet("{index}")]
+        [HttpGet("index[[{index}]]")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            if (index < 0)
+                return BadRequest();
+            if (index >= __Values.Count)
+                return NotFound();
+
+            return __Values[index];
+        }
+
+        [HttpPost]
+        [HttpPost("add")] // POST -> http://localhost:5001/api/values/add
+        public ActionResult Add(string str)
+        {
+            __Values.Add(str);
+            return Ok();
+        }
+
+        [HttpPut("{index}")]
+        [HttpPut("edit/{index}")]
+        public ActionResult Replace(int index, string NewStr)
+        {
+            if (index < 0)
+                return BadRequest();
+            if (index >= __Values.Count)
+                return NotFound();
+
+            __Values[index] = NewStr;
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0)
+                return BadRequest();
+            if (index >= __Values.Count)
+                return NotFound();
+
+            __Values.RemoveAt(index);
+
+            return Ok();
+        }
+    }
+}
