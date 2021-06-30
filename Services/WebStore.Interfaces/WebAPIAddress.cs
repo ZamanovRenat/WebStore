fab6c2a0e31f016364c4c4b64ed5e3995ef8e9bc
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-30 13:52:58 +0300

    АПИКонтроллеры ролей и пользователей для идентити

--- Services/WebStore.Interfaces/WebAPIAddress.cs
+++ Services/WebStore.Interfaces/WebAPIAddress.cs
@@ -5,5 +5,12 @@
         public const string Employees = "api/employees";
         public const string Products = "api/products";
         public const string Orders = "api/orders";
+        //public const object Identity;
+        public static class Identity
+        {
+            public const string Users = "api/users";
+            public const string Roles = "api/roles";
+        }
     }
+   
 }
