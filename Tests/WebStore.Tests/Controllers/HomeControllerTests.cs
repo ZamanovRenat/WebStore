812e0247d31dc076c032a3031dd32301c04d84a4
ZamanovRenat <zamanov.renat@mail.ru>
2021-07-08 20:57:16 +0300

    Добавление тестов для всех методов HomeController

--- Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -21,5 +21,105 @@ namespace WebStore.Tests.Controllers
 
             Assert.IsType<ViewResult>(result);
         }
+        [TestMethod]
+        public void About_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.About();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void Account_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.Account();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void Checkout_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.Checkout();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void Contact_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.Contact();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void MyAccount_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.MyAccount();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void ProductDetails_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.ProductDetails();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void Shop_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.Shop();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void ShopList_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.ShopList();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        [TestMethod]
+        public void WishList_Returns_View()
+        {
+            var configurationMock = new Mock<IConfiguration>();
+
+            var controller = new HomeController(configurationMock.Object);
+
+            var result = controller.WishList();
+
+            Assert.IsType<ViewResult>(result);
+        }
     }
 }
+
