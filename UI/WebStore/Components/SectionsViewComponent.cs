f122a15222273d47baae038ebe690f92676343f2
ZamanovRenat <zamanov.renat@mail.ru>
2021-07-11 12:50:39 +0300

    Выделение активных ссылок секций товаров

--- UI/WebStore/Components/SectionsViewComponent.cs
+++ UI/WebStore/Components/SectionsViewComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
@@ -14,24 +15,47 @@ namespace WebStore.Components
 
         public IViewComponentResult Invoke(string SectionId)
         {
+            var section_id = int.TryParse(SectionId, out var id) ? id : (int?)null;
+
+            var sections = GetSections(section_id, out var parent_section_id);
+
+            ViewBag.SectionId = section_id;
+            ViewData["ParentSectionId"] = parent_section_id;
+
+            return View(new SelectableSectionsViewModel
+            {
+                Sections = sections,
+                SectionId = section_id,
+                ParentSectionId = parent_section_id,
+            });
+        }
+
+        private IEnumerable<SectionViewModel> GetSections(int? SectionId, out int? ParentSectionId)
+        {
+            ParentSectionId = null;
+
             var sections = _ProductData.GetSections();
 
             var parent_sections = sections.Where(s => s.ParentId == null);
 
             var parent_sections_views = parent_sections
-               .Select(s => new SectionViewModel
-               {
-                   Id = s.Id,
-                   Name = s.Name,
-                   Order = s.Order,
-               })
-               .ToList();
+                .Select(s => new SectionViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Order = s.Order,
+                })
+                .ToList();
 
             foreach (var parent_section in parent_sections_views)
             {
                 var childs = sections.Where(s => s.ParentId == parent_section.Id);
 
                 foreach (var child_section in childs)
+                {
+                    if (child_section.Id == SectionId)
+                        ParentSectionId = child_section.ParentId;
+
                     parent_section.ChildSections.Add(new SectionViewModel
                     {
                         Id = child_section.Id,
@@ -39,13 +63,14 @@ namespace WebStore.Components
                         Order = child_section.Order,
                         Parent = parent_section,
                     });
+                }
 
                 parent_section.ChildSections.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
             }
 
             parent_sections_views.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
 
-            return View(parent_sections_views);
+            return parent_sections_views;
         }
     }
 }
